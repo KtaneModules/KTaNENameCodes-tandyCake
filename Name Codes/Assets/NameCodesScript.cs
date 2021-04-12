32b62cf96b7901ac426d15b95bcddd42f5b38265
Danny7007 <67610733+Danny7007@users.noreply.github.com>
2021-04-12 16:14:32 -0400

    Rewrote TP, Cleaned up code, Added Gamepad

--- Name Codes/Assets/NameCodesScript.cs
+++ Name Codes/Assets/NameCodesScript.cs
@@ -2,13 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using UnityEngine;
 using KModkit;
 
 public class NameCodesScript : MonoBehaviour
 {
-
     public KMBombInfo Bomb;
     public KMAudio Audio;
     public KMSelectable leftArrow;
@@ -27,11 +25,9 @@ public class NameCodesScript : MonoBehaviour
     string[] words = new string[] { "ANGLE", "GRAVE", "BARK", "RULER", "KITE", "WHILE", "QUERY", "ARROW", "TEEPEE", "PARTY", "DIJON", "TRAVEL", "BONE", "GREEN", "SPARKS", "SPINS", "VICE", "NOMEN", "VERTIGO", "PRESET", "CRYPT" };
     string chosenString;
     int solution;
-    string[] parameters;
-
 
     void Awake()
-    {
+    {   
         moduleId = moduleIdCounter++;
         leftArrow.OnInteract += delegate () { LeftPress(); return false; };
         rightArrow.OnInteract += delegate () { RightPress(); return false; };
@@ -45,21 +41,18 @@ public class NameCodesScript : MonoBehaviour
         GenerateIndices();
         solution = (2 * leftIndex + rightIndex) % 10;
         Debug.LogFormat("[Name Codes #{0}] Your solution digit is {1}.", moduleId, solution);
-
         displayText.text = currentLetter.ToString();
 
     }
 
     void GenerateIndices()
     {
-        leftIndex = UnityEngine.Random.Range(2, 6);
-        rightIndex = UnityEngine.Random.Range(2, 6);
-
-        if ((Convert.ToDouble(leftIndex) / Convert.ToDouble(rightIndex) % 1 == 0) || (Convert.ToDouble(rightIndex) / Convert.ToDouble(leftIndex) % 1 == 0)) //Checks if the indices are equal or have factors.
+        do
         {
-            GenerateIndices();
-        }
-        else Debug.LogFormat("[Name Codes #{0}] Your left and right indices are {1} and {2}.", moduleId, leftIndex, rightIndex);
+            leftIndex = UnityEngine.Random.Range(2, 6);
+            rightIndex = UnityEngine.Random.Range(2, 6);
+        } while (((double)leftIndex / rightIndex % 1 == 0) || ((double)rightIndex / leftIndex % 1 == 0));
+        Debug.LogFormat("[Name Codes #{0}] Your left and right indices are {1} and {2}.", moduleId, leftIndex, rightIndex);
     }
 
     void GenerateString()
@@ -76,27 +69,22 @@ public class NameCodesScript : MonoBehaviour
         Debug.LogFormat("[Name Codes #{0}] You are starting at position {1}, which is letter {2}.", moduleId, currentPosition + 1, currentLetter);
     }
 
-
     void LeftPress()
     {
-        if (moduleSolved)
-        {
-            return;
-        }
-        Audio.PlaySoundAtTransform("flip", transform);
         leftArrow.AddInteractionPunch(0.2f);
+        Audio.PlaySoundAtTransform("flip", transform);
+        if (moduleSolved)
+            return;
         currentPosition = (currentPosition - leftIndex + stringLength) % stringLength;
         currentLetter = chosenString[currentPosition];
         displayText.text = currentLetter.ToString();
     }
     void RightPress()
     {
-        if (moduleSolved)
-        {
-            return;
-        }
-        Audio.PlaySoundAtTransform("flip", transform);
         rightArrow.AddInteractionPunch(0.2f);
+        Audio.PlaySoundAtTransform("flip", transform);
+        if (moduleSolved)
+            return;
         currentPosition = (currentPosition + rightIndex) % stringLength;
         currentLetter = chosenString[currentPosition];
         displayText.text = currentLetter.ToString();
@@ -104,18 +92,15 @@ public class NameCodesScript : MonoBehaviour
     void SubmitPress()
     {
         if (moduleSolved)
-        {
             return;
-        }
         submitButton.AddInteractionPunch(0.75f);
-        int digitOnSubmit = Mathf.FloorToInt(Bomb.GetTime()) % 10;
+        int digitOnSubmit = ((int)Bomb.GetTime()) % 10;
         if (digitOnSubmit == solution)
         {
-
+            moduleSolved = true;
             Audio.PlaySoundAtTransform("solve", transform);
             if (UnityEngine.Random.Range(0, 100) == 0) { displayText.text = "You are\nJon enough."; displayText.characterSize = 1; displayText.color = new Color(1, 0, 0, 1); }
-            else { displayText.text = "!!"; }   
-            moduleSolved = true;
+            else displayText.text = "!!";   
             Debug.LogFormat("[Name Codes #{0}] You submitted when the last digit of the countdown timer was {1}. Module solved.", moduleId, digitOnSubmit);
             GetComponent<KMBombModule>().HandlePass();
         }
@@ -124,77 +109,60 @@ public class NameCodesScript : MonoBehaviour
             Debug.LogFormat("[Name Codes #{0}] You submitted when the last digit of the countdown timer was {1}. That was incorrect.", moduleId, digitOnSubmit);
             GetComponent<KMBombModule>().HandleStrike();
         }
-
     }
 
     KMSelectable WhichButton(string direction)
     {
-        if (direction == "LEFT" || direction == "L") { return leftArrow; }
+        if (direction == "LEFT" || direction == "L") return leftArrow;
         else return rightArrow;
     }
 
+
 #pragma warning disable 414
-    private readonly string TwitchHelpMessage = @"Use !{0} left 4 to press the left arrow 4 times. Use !{0} submit 5 to press the letter when the last timer digit is 5.";
+    private readonly string TwitchHelpMessage = @"Use !{0} left 4 to press the left arrow 4 times. Directions can be abbreviated. Forgo the number to press the button once. Movements can be chained with spaces. Use !{0} submit 5 to press the letter when the last timer digit is 5.";
 #pragma warning restore 414
 
-    IEnumerator ProcessTwitchCommand(string Command)
+    IEnumerator ProcessTwitchCommand(string input)
     {
-        parameters = Command.Trim().ToUpperInvariant().Split(' ');
-        if (parameters.Length > 2 || parameters.Length < 1)
+        string[] possibleDirections = { "LEFT", "RIGHT", "L", "R" };
+        string Command = input.Trim().ToUpperInvariant();
+        List<string> parameters = Command.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        if (parameters.First() == "SUBMIT" && parameters.Count == 2 && parameters.Last().Length == 1 && "0123456789".Contains(parameters.Last()[0]))
         {
-            yield break;
+            yield return null;
+            int submitting = int.Parse(parameters.Last());
+            while ((int)Bomb.GetTime() % 10 == submitting)
+                yield return "trycancel"; //Fixes the obscure TP bug which got Square Button on ON
+            while ((int)Bomb.GetTime() % 10 != submitting)
+                yield return "trycancel";
+            submitButton.OnInteract();
         }
-        else if (new string[] { "LEFT", "RIGHT", "L", "R" }.Contains(parameters[0]))
+        else if (parameters.Count == 2 && possibleDirections.Contains(parameters[0]) && parameters[1].All(x => "1234567890".Contains(x)))
         {
-            if (parameters.Length == 1)
+            yield return null;
+            for (int i = 0; i < int.Parse(parameters[1]); i++)
             {
-                WhichButton(parameters[0]).OnInteract();
-            }
-            else
-            {
-                if (parameters[1].All(x => "0123456789".Contains(x)) && int.Parse(parameters[1]) <= 50) //Checks if the number is actually a number, and checks that the number is not comically large.
-                {
-                    yield return null;
-                    for (int i = 0; i < int.Parse(parameters[1]); i++)
-                    {
-                        WhichButton(parameters[0]).OnInteract();
-                        yield return new WaitForSeconds(0.75f);
-                        yield return "trycancel";
-                    }
-                }
+                WhichButton(parameters.First()).OnInteract();
+                yield return "trycancel";
+                yield return new WaitForSeconds(0.75f);
             }
         }
-        else if (parameters[0] == "SUBMIT" && parameters.Length == 2)
+        else if (parameters.All(x => possibleDirections.Contains(x)))
         {
-
-            if ("0123456789".Contains(parameters[1]))
+            yield return null;
+            foreach (string command in parameters)
             {
-                yield return null;
-                int submissionDigit = int.Parse(parameters[1]);
-                while (Mathf.FloorToInt(Bomb.GetTime()) % 10 == submissionDigit)
-                {
-                    yield return "trycancel"; //Fixes that obscure bug that got square button on ON.
-                }
-                while (Mathf.FloorToInt(Bomb.GetTime()) % 10 != submissionDigit)
-                {
-                    yield return "trycancel";
-                }
-                submitButton.OnInteract();
-
+                WhichButton(command).OnInteract();
+                yield return "trycancel";
+                yield return new WaitForSeconds(0.75f);
             }
         }
     }
-   
     IEnumerator TwitchHandleForcedSolve()
     {
-        while (!moduleSolved)
-        {
-            while (Mathf.FloorToInt(Bomb.GetTime()) % 10 != solution)
-            {
-                yield return true;
-            }
-            submitButton.OnInteract();
-            yield return new WaitForSeconds(0.05f);
-        }
+        while ((int)Bomb.GetTime() % 10 != solution)
+            yield return true;
+        submitButton.OnInteract();
+        yield return new WaitForSeconds(0.05f);
     }
 }
